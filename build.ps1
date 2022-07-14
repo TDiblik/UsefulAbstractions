560280dfce94ef99ef8160994f274f5a76edec54
Tomáš Diblík <dibla.tomas@post.cz>
2022-07-14 23:28:10 +0200

    [Publish] 1.1.1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 dotnet build ".\TDiblik.UsefulAbstractions\TDiblik.UsefulAbstractions.csproj" -c Release /p:CI=true
 
-dotnet pack ".\TDiblik.UsefulAbstractions\TDiblik.UsefulAbstractions.csproj" --no-build -c Release /p:PackageOutputPath=".\bin\out-nupkg" /p:CI=true /p:Version=1.1.0
+dotnet pack ".\TDiblik.UsefulAbstractions\TDiblik.UsefulAbstractions.csproj" --no-build -c Release /p:PackageOutputPath=".\bin\out-nupkg" /p:CI=true /p:Version=1.1.1
